9657651c43d9fdf8e741d05e43cb624e0614505e
skta.exe <160605627+suuuukita@users.noreply.github.com>
2024-11-20 00:47:31 -0300

    Nexus - Samuel
    Não finalizado

--- Nexus/menu-nexus.js
+++ Nexus/menu-nexus.js
@@ -1,15 +1,18 @@
 let carouselIndex = 0;
 const images = document.querySelectorAll('.carousel-images img');
+const carouselImagesContainer = document.querySelector('.carousel-images');
 
 function showSlides() {
-  carouselIndex++;
-  if (carouselIndex >= images.length) {
-    carouselIndex = 0;
-  }
-  document.querySelector('.carousel-images').style.transform = `translateX(-${carouselIndex * 100}%)`;
+    carouselIndex++;
+    if (carouselIndex >= images.length) {
+        carouselIndex = 0; // Volta para o início após a última imagem
+    }
+    // Move o carrossel para mostrar a próxima imagem
+    carouselImagesContainer.style.transform = `translateX(-${carouselIndex * 100}%)`;
 }
 
-setInterval(showSlides, 3000); // Muda de imagem a cada 3 segundos
+// Altere a imagem a cada 3 segundos
+setInterval(showSlides, 10000);
 
 // Script para menu hamburguer
 const hamburgerButton = document.getElementById('hamburgerButton');
